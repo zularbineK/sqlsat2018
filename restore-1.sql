c5b39e161d145629a785c3ebb98af2093427988b
zularbine <zularbine@arditesbd.com>
2018-11-23 17:16:29 +0600

    test

--- restore-1.sql
+++ restore-1.sql
@@ -1,7 +1,7 @@
 USE [master]
 ALTER DATABASE [dummydb] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
 RESTORE DATABASE [dummydb] FROM  URL = N'https://sqlserverbackup101.blob.core.windows.net/sqlsat2018/Non_encrypted_d1_backup_20181123110234.bak'
-WITH  CREDENTIAL = N'[https://sqlserverbackup101.blob.core.windows.net/sqlsat2018]'
+WITH  CREDENTIAL = N'https://sqlserverbackup101.blob.core.windows.net/sqlsat2018'
 ,  FILE = 1,  NOUNLOAD,  STATS = 5
 ALTER DATABASE [dummydb] SET MULTI_USER
 GO
